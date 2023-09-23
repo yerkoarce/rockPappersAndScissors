e5baf6ee42e7e97bad8d57b5775d402a0c9ef347
yerko arce <yerko.arce.galaz@gmail.com>
2023-09-22 21:03:34 -0300

    arreglo a la funcion playRound y aun intentando unir la funcion game con el resto de funciones

--- pruebas.js
+++ pruebas.js
@@ -1,3 +1,13 @@
-let num = Math.floor(Math.random() * 3);
+function playRound(playerSelection , computerSelection) {
 
-console.log(num)
+    if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper")){
+        console.log(3); //wins
+    } else if ((playerSelection == "paper" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "scissors")){
+        console.log(2); //ties
+    } else {
+        console.log(1); //lost
+    }
+    
+}
+
+console.log(playRound("scissors","rock"))
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function getComputerChoice () {
     let num = Math.floor(Math.random() * 3);
     if (num === 0) {
-        return "Rock";
+        return "rock";
     } else if (num === 1) {
-        return "Paper";
+        return "paper";
     } else if (num === 2) {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 
 
 function playRound(playerSelection , computerSelection) {
-    
-    console.log(playerSelection);
-    console.log(computerSelection);
-    if (playerSelection === computerSelection) {
-        return "Tie!";
-    } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'scissors' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        return "You lost!";
+
+    let computerSelection = getComputerChoice();
+
+    if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper")){
+        return 3; //wins
+    } else if ((playerSelection == "paper" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "scissors")){
+        return 2; //ties
     } else {
-        return "You win!";
+        return 1; //lost
     }
+    
 }
 
 function game(){
@@ -29,22 +30,33 @@ function game(){
     let lose = 0;
     let ties = 0;
 
-    for (let i = 1 ; i < 6 ; i++) {
-        if (playRound(playerSelection,computerSelection) === "You win!") {
-            wins++
-        } else if (playRound(playerSelection,computerSelection) === "You lost!") {
-            lose++
-        } else if (playRound(playerSelection,computerSelection) === "Tie!") {
+    for (i = 0 ; i < 5 ; i++){
+
+        
+    
+        let playerSelection = prompt("Choose your move: ")
+
+        if (playRound(playerSelection,computerSelection) === 2) {
             ties++
+        } else if (playRound(playerSelection,computerSelection) === 1) {
+            lose++
+        } else if (playRound(playerSelection,computerSelection) === 3) {
+            wins++
         }
     }
+
+    console.log(wins)
+    console.log(lose)
+    console.log(ties)
+    /*
     if (wins > lose) {
         return "You win!";
     } else if (wins < lose) {
         return "You lost";
     }
+    */
 }
-let playerSelection = "paper";
-let computerSelection = getComputerChoice();
+
+
 game();
 
